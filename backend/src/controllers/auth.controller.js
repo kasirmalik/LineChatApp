05b31239a5d71ccdea51e253e524b24e9f0d0b50
Kaiser Fayaz <kasirmalik87@gmail.com>
2025-02-13 17:23:35 +0530

    add checkAuth controller and fix token retrieval in protectRoute middleware; create message model

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -91,4 +91,13 @@ export const updateProfile = async (req,res) =>{
   }
 }
 
+export const checkAuth = (req,res)=>{
+  try {
+    res.status(200).json(req.user)
+  } catch (error) {
+    console.log("Error in checkAuth controller",error.message)
+    res.status(500).json({message:"internel server error"})
+  }
+}
+
 // UGnAGnl2AXfduVuK
--- backend/src/middleware/auth.middleware.js
+++ backend/src/middleware/auth.middleware.js
@@ -3,12 +3,11 @@ import  User  from '../models/user.model.js';
 
 export const protectRoute = async (req,res,next)=>{
     try {
-        const token = req.cookie.jwt;
+        const token = req.cookies.jwt;
         if (!token) {
             return res.status(401).json({message:"Unauthroized -- No token provided"});
         }
-
-        const decoded = jwt.verify(process.env.JWT_SECRET)
+        const decoded = jwt.verify(token,process.env.JWT_SECRET)
         if (!decoded) {
             return res.status(401).json({message:"Unauthroized -- Invalid token"});
         }

e5d27ac30807b6f30dbaaf4d202b314be0a52672
Kaiser Fayaz <kasirmalik87@gmail.com>
2025-02-14 16:25:58 +0530

    add Tailwind CSS setup, create Navbar and authentication pages; update App component for routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
 
 function App() {
-
-
   return (
     <>
-      <h1 c>hello</h1>
+     <div>
+        <Navbar/>
+
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/signup" element={<SignUpPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/settings" element={<SettingsPage />} />
+          <Route path="/profile" element={<ProfilePage />} />
+        </Routes>
+     </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
